837e990e97a2529791098e8c56dfe30cfeb56adc
tz <tz@qq.com>
2022-11-15 16:53:43 +0800

    third commit

--- src/main/java/com/tz/Test.java
+++ src/main/java/com/tz/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("666");
         System.out.println("66622");
+        System.out.println("66622");
     }
 }
