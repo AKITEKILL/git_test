644d618c1f337ee1315e26066b210343b826afa7
tz <tz@qq.com>
2022-11-15 17:07:06 +0800

    hot-fox commit

--- src/main/java/com/tz/Test.java
+++ src/main/java/com/tz/Test.java
@@ -5,7 +5,6 @@ public class Test {
         System.out.println("666");
         System.out.println("66622");
         System.out.println("66622");
-
         System.out.println("hot-fox");
     }
 }
